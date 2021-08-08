55cff83bb139486c41e0888cb74a151c75a98acb
Bartosz Prochowski <bravoprochu@gmail.com>
2021-08-08 18:53:09 +0200

    back to iso date format

--- bp.kpir.entities/Invoice/PaymentTermsDTO.cs
+++ bp.kpir.entities/Invoice/PaymentTermsDTO.cs
@@ -4,12 +4,16 @@ namespace bp.kpir.DAO.Invoice
 {
     public class PaymentTermsDTO
     {
-        public string PaymentTermsCombined { get {
+        public string PaymentTermsCombined
+        {
+            get
+            {
 
-                string description = this.PaymentTerm.IsDescription ? ", "+ this.Description : null;
-                string paymentDate = this.PaymentTerm.IsPaymentDate ? $", {PaymentDays.Value} dni,  termin płatności: " + this.PaymentDate.Value.ToShortDateString() : null;
+                string description = this.PaymentTerm.IsDescription ? ", " + this.Description : null;
+                string paymentDate = this.PaymentTerm.IsPaymentDate ? $", {PaymentDays.Value} dni,  termin płatności: " + this.PaymentDate.Value.ToLocalTime().ToShortDateString() : null;
                 return $"{this.PaymentTerm.Name}{paymentDate}{description}";
-            }}
+            }
+        }
         public string Description { get; set; }
         public DateTime Day0 { get; set; }
         public int PaymentTermsId { get; set; }
